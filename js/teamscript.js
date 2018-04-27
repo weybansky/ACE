8f6e9483047ed7b3937efb1d5656fa4649e3bce5
Kehinde Faleye <kenny.fale.kf@gmail.com>
2018-04-27 03:20:52 +0100

    fixed download page

---
+++ js/teamscript.js
@@ -0,0 +1,28 @@
+function page1(){
+	$('.page2').css('display','none');
+	$('.tab2').removeClass('active');
+	$('.page3').css('display','none');
+	$('.tab3').removeClass('active');
+	$('.page1').css('display','inline-block');
+	$('.tab1').addClass('active');
+}
+
+function page2(){
+
+	$('.page1').css('display','none');
+	$('.tab1').removeClass('active');
+	$('.page3').css('display','none');
+	$('.tab3').removeClass('active');
+	$('.page2').css('display','inline-block');
+	$('.tab2').addClass('active');
+}
+
+function page3(){
+
+	$('.page1').css('display','none');
+	$('.tab1').removeClass('active');
+	$('.page2').css('display','none');
+	$('.tab2').removeClass('active');
+	$('.page3').css('display','inline-block');
+	$('.tab3').addClass('active');
+}
